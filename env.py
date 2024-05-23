959b4ef33a8f170f17d7087793b0960b2f74fcd4
Basel Hussein <cv98@windowslive.com>
2024-05-23 11:19:49 +0000

    deleted env.py

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "<your-database-URL>")
+os.environ.setdefault(
+    "SECRET_KEY", "pbkdf2_sha256$600000$IkZtx7hFoEk6TaQaxjJwch$Bzzc+OjZUYAIWaVClRItOj+PGjQGYy+939sFptCgq4s=")
